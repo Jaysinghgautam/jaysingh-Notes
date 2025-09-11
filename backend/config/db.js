dca8f43f06553325b20e92957bc8867be0be21f5
Jaysinghgautam <gjaysingh53@gmail.com>
2025-09-11 18:07:27 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
  import mongoose from "mongoose";
 
 const DbCon=async()=>{
-    try {
-        mongoose.connect(process.env.MONGODB_URI)
-        console.log('Mongodb is connected')
-        
-    } catch (error) {
-        console.log("Error in mongodb connection",error)
-    }
+  try {
+  await mongoose.connect(`${process.env.MONGODB_URI}/rani`);
+  console.log("Mongodb is connected");
+} catch (error) {
+  console.log("Error in mongodb connection", error);
+}
+
 }
 export default DbCon
--- frontend/src/services/ApiEndPoint.js
+++ frontend/src/services/ApiEndPoint.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'http://localhost:5000',
-    headers: {
-        'Content-Type': 'application/json' // Removed extra space
-    },
-    withCredentials: true
+ const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
+
 
 export const get = (url, params) => instance.get(url, { params });
 export const post = (url, data) => instance.post(url, data);
